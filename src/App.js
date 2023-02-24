797d58c5fab75c3adcf5ff4800f716b22d73e76c
Arif uz-zaman <arif.uz-zaman@roche.com>
2023-02-24 12:37:05 +0600

    redme file edited

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { AddNewTransition } from "./components/AddNewTransition";
+import { Balance } from "./components/Balance";
+import { Header } from "./components/Header";
+import { IncomeExpence } from "./components/IncomeExpence";
+import { TransitionList } from "./components/TransitionList";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Header title="Expence Tracker" />
+      <div className="container">
+        <Balance />
+        <IncomeExpence />
+        <TransitionList />
+        <AddNewTransition />
+      </div>
+    </div>
+  );
 }
 
 export default App;
